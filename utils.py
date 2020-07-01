f470be457e106d029352646cb03f3696b11bd05c
nathan <ing.nathany@gmail.com>
2020-07-01 10:50:08 -0700

    first scratch commit

---
+++ utils.py
@@ -0,0 +1,30 @@
+import logging
+
+
+def make_logger():
+  # Call this one at the op of scripts
+  logger = logging.getLogger("ccsig")
+  logger.setLevel(logging.INFO)
+  logger.propagate = False
+
+  # remove all default handlers
+  for handler in logger.handlers:
+      logger.removeHandler(handler)
+
+  # create console handler and set level to debug
+  console_handle = logging.StreamHandler()
+  console_handle.setLevel(logging.INFO)
+
+  # create formatter
+  formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(module)s %(lineno)s: %(message)s')
+  console_handle.setFormatter(formatter)
+
+  # now add new handler to logger
+  logger.addHandler(console_handle)
+  return logger
+
+
+def get_logger(app_name='ccsig'):
+  # Call this one inside modules
+  logger = logging.getLogger(app_name)
+  return logger
